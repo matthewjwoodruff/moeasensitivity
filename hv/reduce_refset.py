e0dc876ed4775b1d27e8f636d26b91837d1db45c
Matthew Woodruff <mjw5407@psu.edu>
2013-04-23 10:56:10 -0400

    Clarified contributors
    Shouldn't copy-paste that stuff!

--- hv/reduce_refset.py
+++ hv/reduce_refset.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2013 Joseph Kasprzyk, Matthew Woodruff and others.
+Copyright (C) 2013 Matthew Woodruff 
 
 This script is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
