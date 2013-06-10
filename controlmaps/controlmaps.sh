e6694497fb224ef792e81472144b1d48c22bd92d
Matthew Woodruff <mjw5407@psu.edu>
2013-06-10 13:34:55 -0400

    Added call to imagemagick
    Using -trim +repage to auto-crop the whitespace
    around the figures.

--- controlmaps/controlmaps.sh
+++ controlmaps/controlmaps.sh
@@ -2,3 +2,7 @@ python controlmaps.py Borg,eMOEA,NSGAII,eNSGAII,GDE3 27_10_1.0 -o 27_10_1.0.png 
 python controlmaps.py Borg,eMOEA,NSGAII,eNSGAII,GDE3 18_10_1.0 -o 18_10_1.0.png -w 0.00389732705522 -b 0.0390303319163
 python controlmaps.py Borg,eMOEA,NSGAII,eNSGAII,GDE3 27_3_0.1 -o 27_3_0.1.png -w 0.0522599393295 -b 0.0824581625125
 python controlmaps.py Borg,eMOEA,NSGAII,eNSGAII,GDE3 18_3_0.1 -o 18_3_0.1.png -w 0.0522599393295 -b 0.0824581625125
+convert 27_10_1.0.png -trim +repage 42_27_10.png
+convert 18_10_1.0.png -trim +repage 43_18_10.png
+convert 27_3_0.1.png -trim +repage 40_27_3.png
+convert 18_3_0.1.png -trim +repage 41_18_3.png
