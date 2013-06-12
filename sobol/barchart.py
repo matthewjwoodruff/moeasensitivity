11a6c104b244e3fc28f6b326d054025b4af1a437
Matthew Woodruff <mjw5407@psu.edu>
2013-06-12 12:43:13 -0400

    Implemented key
    Depending on the barchart, you may have to tweak positions by hand.

--- sobol/barchart.py
+++ sobol/barchart.py
@@ -29,15 +29,80 @@ import matplotlib
 from matplotlib.backends import backend_agg as agg
 from matplotlib.backends import backend_svg as svg
 
-def barchart_figure(fig, table, algos, key=False):
+def make_labels(ax, labels):
+    """
+    Draw the labels as follows:
+    two labels: 1/4, 3/4
+    three labels: 1/6, 3/6, 5/6
+    four labels: 1/8, 3/8, 5/8, 7/8
+    five labels: 1/10, 3/10, 5/10, 7/10, 9/10
+    """
+    denominator = 2.0 * len(labels)
+    fractions = [(2.0*xx + 1) / denominator
+                 for xx in range(len(labels))]
+    for label, yy in zip(labels, fractions):
+        ax.text(0.5, yy, label, 
+                horizontalalignment="center",
+                verticalalignment="center",
+                rotation="vertical", size="small")
+    ax.set_yticks([])
+
+    step = 1.0 / float(len(labels))
+    for yy in range(1, len(labels)):
+        ax.axhline(yy * step, color = 'k')
+
+def draw_key(ax):
+    """
+    Make a key
+    """
+    ax.set_xlim((0,3))
+    ax.set_ylim((0,4))
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    colors = {"Total": (0.2,0.2,0.2), "First": (0.9,0.9,0.9)}
+    widths = {"Total": 0.8, "First": 0.6}
+    zorders = {"Total": 0, "First": 1}
+
+    x1 = 2
+    x2 = 0
+    for order, val in [("First", 0.6), ("Total", 0.8)]:
+        rect = matplotlib.patches.Rectangle(
+            (0.6, 1.5),
+            #(1-widths[order]/2, 1.5),
+            widths[order], val, facecolor=colors[order], 
+            edgecolor=(1,1,1),
+            zorder = zorders[order])
+        x1 = min(x1, 1-widths[order]/2)
+        x1 = 0.6
+        #x2 = max(x2, 1+widths[order]/2)
+        x2 = max(x2, 0.3+widths[order])
+        ax.add_patch(rect)
+    line = matplotlib.lines.Line2D([x1,x2], [1.5,1.5], color='k')
+    #ax.add_line(line)
+    ax.set_frame_on(False)
+    ax.annotate("Total\nOrder", (0.8, 2.3), xytext=(1.1, 2.45),
+                horizontalalignment="baseline",
+                arrowprops={"width":0.3, "shrink":0.05,
+                            "frac": 0.1, "headwidth":4})
+    ax.annotate("First\nOrder", (0.8, 1.6), xytext=(1.1, 1.2),
+                horizontalalignment="baseline",
+                arrowprops={"width":0.3, "shrink":0.05,
+                            "frac": 0.1, "headwidth":4})
+    ax.text(1.3, 2.8, "Key", horizontalalignment="center",
+            weight="bold")
+    rect = matplotlib.patches.Rectangle((0.25, 1.1), 2.3, 1.85,
+                facecolor='w', edgecolor='k', zorder=-1)
+    ax.add_patch(rect)
+        
+
+def barchart_figure(fig, table, algos, key=False, **kwargs):
     """
     Make four stacked horizontal barcharts for each
     algorithm.  Put algorithms one after another.
     Draw a key if requested.
     There will be a lot of slack at the top and bottom
     for labels.
-    Return the number of bars wide the figure is, so 
-    that it can be sized intelligently.
     """
     table = clean(table)
 
@@ -45,37 +110,52 @@ def barchart_figure(fig, table, algos, key=False):
     counts = table.groupby(["algo", "input"]).count()
     nbars = [len(counts.ix[algo]) for algo in algos]
     total_nbars = sum(nbars)
+    total_nbars += 1 # for the labels on the left
     if key:
-        total_nbars += 2
+        total_nbars += 3
     nbars = dict(zip(algos, nbars))
 
+    wbar = 1.0 / total_nbars
+
     # add objective and decision variable lables
-    objlabels = fig.add_axes((0, 0.5, 0.1, 0.4))
-    objlabels.set_title("Objectives")
-    dvlabels = fig.add_axes((0.1, 0.5, 0.1, 0.4))
-    dvlabels.set_title("Decision\nVariables")
+    objlabels = fig.add_axes((0, 0.5, 0.5*wbar, 0.4))
+    make_labels(objlabels, ["10 Objectives", "3 Objectives"])
+    objlabels.set_xticks([])
+    objlabels.tick_params(axis="x", bottom=False, top=False)
+
+    dvlabels = fig.add_axes((0.5*wbar, 0.5, 0.5*wbar, 0.4))
+    make_labels(dvlabels, ["18 DV", "27 DV", "18 DV", "27 DV"])
+    dvlabels.set_xticks([])
+    dvlabels.tick_params(axis="x", bottom=False, top=False)
 
     # add actual bar charts
     table = table.set_index(["algo", "problem", "input", "order"])
-    xx = 0
+    xx = wbar # offset for the labels on the left
 
     for algo in algos:
-        width = float(nbars[algo]) / float(total_nbars)
-        left = scalebetween(0.2, 1.0, xx)
-        offset = scaleby(0.2,1.0,width)
-        ax = fig.add_axes((left, 0.5, offset, 0.4))
-        ax.set_title(algo)
+        width = nbars[algo] * wbar
+        ax = fig.add_axes((xx, 0.5, width, 0.4))
+        ax.set_title(algo, weight="bold")
         barchart_axes(ax, table.ix[algo])
         xx += width
 
-    return total_nbars
+    if key:
+        ax = fig.add_axes((xx, 0.5, 3*wbar, 0.4))
+        draw_key(ax)
+
+    # adjust bar width and height
+    barwidth = kwargs.get("barwidth", 0.4)
+    barheight = kwargs.get("barheight", 1.3)
+    fig.set_figwidth(total_nbars * barwidth)
+    fig.set_figheight(10*barheight)
 
 def bar(ax, xx, yy, order, val):
-    color = {"Total": (0.2,0.2,0.2), "First": 'w'}[order]
-    width = {"Total": 0.8, "First": 0.7}[order]
+    color = {"Total": (0.2,0.2,0.2), "First": (0.9,0.9,0.9)}[order]
+    width = {"Total": 0.8, "First": 0.6}[order]
     zorder = {"Total": 0, "First": 1}[order]
     rect = matplotlib.patches.Rectangle(
-        (xx + 0.5*(1.0-width), yy),
+        #(xx + 0.5*(1.0-width), yy),
+        (xx + 0.1, yy),
         width, val, facecolor = color, edgecolor = (0,0,0,0),
         zorder = zorder)
     ax.add_patch(rect)
@@ -109,7 +189,11 @@ def barchart_axes(ax, table):
         yy += 1 
 
     ax.set_xticks([xx + 0.5 for xx in range(len(inputs))])
-    ax.set_xticklabels(inputs, rotation="vertical")
+    ax.set_xticklabels(inputs, rotation="vertical", size="small" )
+    for tl in ax.xaxis.get_majorticklabels():
+        pos = tl.get_position()
+        pos = (pos[0], -0.0025)
+        tl.set_position(pos)
     ax.tick_params(axis="x", bottom=False, top=False)
     ax.set_yticks([])
 
@@ -121,20 +205,31 @@ def get_args():
     parser.add_argument("algos", 
                         help = "Comma-separated list of MOEAS "\
                                "to include in the plot") 
+    parser.add_argument("output", help="name of image file to write")
     parser.add_argument("-k", "--key", action="store_true",
                         help = "Make a key")
     parser.add_argument("-W", "--width", type=float, default=0.25,
                         help = "Width in inches of a bar")
     parser.add_argument("-H", "--height", type=float, default=1.0,
                         help = "Maximum height in inches of a bar.")
+    parser.add_argument("-v", "--vector", action="store_true",
+                        help = "Write SVG instead of PNG output")
     return parser.parse_args()
 
 def cli():
     args = get_args()
     table = pandas.read_table(args.table, sep=' ')
     fig = matplotlib.figure.Figure()
+    if args.vector:
+        svg.FigureCanvasSVG(fig)
+    else:
+        agg.FigureCanvasAgg(fig)
+    
     algos = args.algos.split(",")
-    barchart_figure(fig, table, algos, args.key)
+    barchart_figure(fig, table, algos, args.key,
+                    barwidth=args.width,
+                    barheight=args.height)
+    fig.savefig(args.output)
 
 if __name__ == "__main__":
     cli()
