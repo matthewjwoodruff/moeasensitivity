1e2ed483f9faa01a901115e9d5bbcc31090fe692
Matthew Woodruff <mjw5407@psu.edu>
2013-06-13 11:24:06 -0400

    Now also spit out svgs

--- sobol/barchart.sh
+++ sobol/barchart.sh
@@ -1,4 +1,6 @@
 python barchart.py sens Borg borg -k  -W 0.35 -H 1.0
 python barchart.py sens eMOEA,NSGAII,eNSGAII,GDE3 others -W 0.35 -H 1.0
+python barchart.py sens Borg borg -k -W 0.35 -H 1.0 -v
+python barchart.py sens eMOEA,NSGAII,eNSGAII,GDE3 others -W 0.35 -H 1.0 -v
 convert borg.png -trim +repage 50_borg.png
 convert others.png -trim +repage 51_others.png
