9020e7486271a0b14a27df9afdabfebc67ccdcdc
Matthew Woodruff <mjw5407@psu.edu>
2014-02-23 23:36:34 -0500

    For refset hv

---
+++ hv/doit.sh
@@ -0,0 +1,2 @@
+java -Xmx1g -server -classpath lib/commons-cli-1.2.jar:lib/commons-codec-1.5.jar:lib/commons-lang3-3.1.jar:lib/commons-math-2.2.jar:lib/jcommon-1.0.16.jar:lib/jfreechart-1.0.13.jar:lib/JMetal-3.1.jar:lib/MOEAFramework-1.15-Proprietary.jar:lib/MOEAFramework-1.17.jar org.moeaframework.analysis.sensitivity.ResultFileEvaluator --dimension 3 --output tempout --input tempin --reference 273.ref
+
